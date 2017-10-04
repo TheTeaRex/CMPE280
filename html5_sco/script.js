89c4ccb7e25a0d8102089bbb21e0327b0b84a9c8
TheTeaRex <thetearex10@gmail.com>
2017-10-03 17:32:55 -0700

    html5_sco added script.js

---
+++ html5_sco/script.js
@@ -0,0 +1,45 @@
+function show1() {
+  document.getElementById('englishchoice1').style.display = 'block';
+  document.getElementById('englishchoice2').style.display = 'none';
+}
+function show2() {
+  document.getElementById('englishchoice2').style.display = 'block';
+  document.getElementById('englishchoice1').style.display = 'none';
+}
+
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function setHiddenValues() {
+  var mresult = parseInt(getParameterByName('math')) || 0;
+  var cresult = parseInt(getParameterByName('cosine')) || 0;
+  var e1result = parseInt(getParameterByName('mark1')) || 0;
+  var e2result = parseInt(getParameterByName('mark2')) || 0;
+  var vresult = parseInt(getParameterByName('video')) || 0;
+  var quantitative = parseInt(getParameterByName('quantitative')) || 0;
+  var reading = parseInt(getParameterByName('reading')) || 0;
+  var videoaudio = parseInt(getParameterByName('videoaudio')) || 0;
+  quantitative = quantitative + mresult + cresult;
+  reading = reading + e1result + e2result;
+  videoaudio = videoaudio + vresult;
+  document.getElementById('quantitative').value = quantitative;
+  document.getElementById('reading').value = reading;
+  document.getElementById('videoaudio').value = videoaudio;
+  console.log(document.getElementById('quantitative').value);
+}
+
+function getHiddenValues() {
+  var quantitative = parseInt(getParameterByName('quantitative')) || 0;
+  var reading = parseInt(getParameterByName('reading')) || 0;
+  var videoaudio = parseInt(getParameterByName('videoaudio')) || 0;
+  document.getElementById('quantitative').innerHTML = quantitative;
+  document.getElementById('reading').innerHTML = reading;
+  document.getElementById('videoaudio').innerHTML = videoaudio;
+}
