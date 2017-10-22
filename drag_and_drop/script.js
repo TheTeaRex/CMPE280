b728a02e1833987b0514c9c6be145980fd8ba26f
TheTeaRex <thetearex10@gmail.com>
2017-10-21 22:38:28 -0700

    drag and drop: added dropping anywhere within the div

--- drag_and_drop/script.js
+++ drag_and_drop/script.js
@@ -1,13 +1,23 @@
-function allowDrop(evt) {
-  evt.preventDefault();
+function drag_over(event) {
+  event.preventDefault();
 }
 
-function drag(evt) {
-  evt.dataTransfer.setData("text", evt.target.id);
+function drag(event) {
+  var style = window.getComputedStyle(event.target, null);
+  var data = (parseInt(style.getPropertyValue("left"),10) - event.clientX) + ',' + (parseInt(style.getPropertyValue("top"),10) - event.clientY) + ',' + event.target.id;
+  event.dataTransfer.setData("text", data);
+  console.log(style.getPropertyValue("left"));
+  console.log(style.getPropertyValue("top"));
 }
 
-function drop(evt) {
-  evt.preventDefault();
-  var data = evt.dataTransfer.getData("text");
-  evt.target.appendChild(document.getElementById(data));
+function drop(event) {
+  var data = event.dataTransfer.getData("text").split(',');
+  var l = (event.clientX + parseInt(data[0])) + 'px';
+  var t = (event.clientY + parseInt(data[1])) + 'px';
+  console.log(l);
+  console.log(t);
+  var object = document.getElementById(data[2]);
+  object.style.setProperty('left', l);
+  object.style.setProperty('top', t);
+  event.preventDefault();
 }
